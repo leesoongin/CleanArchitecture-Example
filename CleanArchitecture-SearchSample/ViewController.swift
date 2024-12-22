0581cb68c1dd5508b2b6c67988e2e5c08c9b92d5
mumu <tnddls2ek@naver.com>
2024-12-22 17:07:53 +0900

    Initial Commit

---
+++ CleanArchitecture-SearchSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanArchitecture-SearchSample
+//
+//  Created by 이숭인 on 12/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
